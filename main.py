6fe16f600869f9c75d15e2605923c47576c19917
Rainer rei Schuth <rei@reixd.net>
2024-04-25 23:07:22 +0200

    Add flexibility to example

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from solvis_sc3_modbus.client import SolvisSC3ModbusClient
 host = os.getenv('SOLVIS_HOST', 'localhost')
 port = int(os.getenv('SOLVIS_PORT', 502))
 unit_id = int(os.getenv('SOLVIS_UNIT_ID', 101))
+register_address = int(os.getenv('SOLVIS_REG_ADDRESS', 33024))
 
 def main():
     solvis_client = SolvisSC3ModbusClient(host=host, port=port, unit_id=unit_id, debug=False)
     if solvis_client.connect():
-        data = solvis_client.fetch_data(register_address=33024, length=1)
+        data = solvis_client.fetch_data(register_address=register_address, length=1)
         print("Data from registers:", data)
     else:
         print("Connection to Solvis SC3 device failed.")
